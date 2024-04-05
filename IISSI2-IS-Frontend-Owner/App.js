101921401db789a2da882ce25b1f0021393f393f
lurio84 <lucas.ronquillo@gmail.com>
2024-04-05 15:39:57 +0200

    Sync

---
+++ IISSI2-IS-Frontend-Owner/App.js
@@ -0,0 +1,26 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native'
+import * as React from 'react'
+import ControlPanelScreen from './src/screens/controlPanel/ControlPanelScreen'
+import ProfileScreen from './src/screens/profile/ProfileScreen'
+import RestaurantsStack from './src/screens/restaurants/RestaurantStack'
+
+const Tab = createBottomTabNavigator()
+
+export default function App () {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Restaurants"
+          component={RestaurantsStack} />
+        <Tab.Screen
+          name="Control Panel"
+          component={ControlPanelScreen} />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
---
+++ IISSI2-IS-Frontend-Owner/src/screens/controlPanel/ControlPanelScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import SystemInfo from '../../components/SystemInfo'
+
+export default function ControlPanelScreen () {
+  return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <SystemInfo/>
+        </View>
+  )
+}
